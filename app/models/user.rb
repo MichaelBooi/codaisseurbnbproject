56df0b8630b10ea9274c8d67dd2efccd12a67feb
MichaelBooi <michaelbooi@hotmail.com>
2016-10-12 11:17:24 +0200

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_one :profile
+
+  def has_profile?
+    profile.present?
+  end
+
+  def full_name
+    profile.full_name
+  end
 end
